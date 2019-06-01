5c94ebe708ce77fa2b3b7dd986613dc01e76fb39
codejoncode <jonathanjholloway@gmail.com>
2019-06-01 13:07:19 -0500

    filtering by clicking the tab now functional appears to be working correctly after a few manual tests

--- client/src/Components/Projects/Projects.js
+++ client/src/Components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Menu } from "semantic-ui-react";
+import { Grid, Menu, Header } from "semantic-ui-react";
 import { technologiesData, projectData  } from "../../Helpers/projectData";
 import ProjectSection from "./ProjectSection";
 
@@ -8,39 +8,64 @@ import ProjectSection from "./ProjectSection";
 class Projects extends Component {
   state = {
     columnCount: 3,
-    activeItem: "ALL"
+    activeItem: "ALL",
+    projectsDisplay: []
   };
 
-  handleItemClick = (e, { name }) => () => {
-    this.setState({activeItem: name.toUpperCase()})
+  componentDidMount () {
+    this.filterData("ALL");
   }
 
-  render() {
+  filterData = async (name) => {
     const rowCount = Math.ceil(projectData.length / this.state.columnCount);
     const projectsDisplay = [];
     let projectCount = 0;
 
     for (let i = 0; i < rowCount; i++) {
       let setOfColumns = [];
-
-      for (let j = projectCount; j < projectCount + 3; j++) {
-        if (projectData[j]) {
-          setOfColumns.push(projectData[j]);
+      while (projectCount < projectData.length-1){
+        if (projectData[projectCount]) {
+          const tech = projectData[projectCount].technologies.toUpperCase();
+          if (this.state.activeItem === "ALL" || tech.includes(this.state.activeItem) === true){
+            setOfColumns.push(projectData[projectCount]);
+            if(setOfColumns.length === 3) {
+              projectCount += 1; 
+              break;
+            }
+          }
         }
+        projectCount += 1 
       }
-      projectsDisplay.push(setOfColumns);
-      projectCount += 3;
+      if(setOfColumns.length > 0){
+        projectsDisplay.push(setOfColumns);
+      }
+      if (projectCount >= projectData.length){
+        break;
+      }
+
     }
     console.log(projectsDisplay);
-    const { activeItem } = this.state;
+    this.setState({projectsDisplay, activeItem: name})
+  }
+
+
+  handleItemClick =  (e, { name }) => {
+    this.setState({activeItem : name}, () => this.filterData(name.toUpperCase()))
+  }
+
+  render() {
+    
+    const { activeItem, projectsDisplay } = this.state;
     const technologies = ["ALL", ...technologiesData]
+    console.log(`The activeItme is : ${activeItem}`)
+    console.log(projectsDisplay)
     return (
       <div style={{ margin: "20px" }}>
-        {/* <Tab menu={{ pointing: true }} panes={panes} fluid stackable /> */}
+        <Header style={{ margin: "20px", textAlign: "center" }}>Click a tab to filter the projects list.</Header>
         <Menu pagination fluid stackable>
-          {technologies.map((tech, index) => <Menu.Item name = {tech.toUpperCase()} active={activeItem === tech.toUpperCase()} onClick = {this.handleItemClick}></Menu.Item>)}
+          {technologies.map((tech, index) => <Menu.Item key = {index} name = {tech.toUpperCase()} active={activeItem === tech.toUpperCase()} onClick = {this.handleItemClick}></Menu.Item>)}
         </Menu>
-        <br/>
+        <Header style = {{textAlign: "center", margin: "20px"}}>Click a project for more information.</Header>
         <Grid style = {{margin: "20px"}}columns={this.state.columnCount} divided stackable>
           {projectsDisplay.map((projects, index) => (
             <ProjectSection key={index} projects={projects} />
