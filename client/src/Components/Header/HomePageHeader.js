688ca275c841b6c88cbf9b2c566d384cdf135451
codejoncode <jonathanjholloway@gmail.com>
2019-06-03 16:48:25 -0500

    home page header coming along nicely have three cards giving a quick overview of my skills and abilities

--- client/src/Components/Header/HomePageHeader.js
+++ client/src/Components/Header/HomePageHeader.js
@@ -1,102 +1,232 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Grid, Header, Segment, Item, Icon, Card} from "semantic-ui-react";
+import {
+  Grid,
+  Header,
+  Segment,
+  Item,
+  Icon,
+  Card,
+  List, 
+  Container, 
+} from "semantic-ui-react";
 import profilePic from ".././../Images/profile pic.PNG";
 import IntroductionSkills from "../Skills/IntroductionSkills";
 
-import {lighterBlue, darkBlack} from "../../Helpers/Colors/colors";
+import { lighterBlue, darkBlack } from "../../Helpers/Colors/colors";
 
 class HomePageHeader extends Component {
   render() {
     return (
-      <Grid.Column width={16} >
-        <Segment style={{backgroundColor: darkBlack}} textAlign='center'>
+      <Container>
+      <Grid.Column width={16}>
+        <Segment style={{ backgroundColor: darkBlack }} textAlign="center">
           <Item.Group>
-              <Item.Image  src={profilePic} centered/>
+            <Item.Image src={profilePic} centered />
             <Item>
               <Item.Content verticalAlign="bottom">
-                <Header id = "lighterBlue" as="h1">Jonathan J. Holloway</Header>
+                <Header id="lighterBlue" as="h1">
+                  Jonathan J. Holloway
+                </Header>
                 <br />
-                <Header id = "lighterBlue" as = "h3">Software Engineer</Header>
-                <br/>
-                <Header id = "lighterBlue" as="h5">Crown Point, IN</Header>
+                <Header id="lighterBlue" as="h3">
+                  Software Engineer
+                </Header>
+                <br />
+                <Header id="lighterBlue" as="h5">
+                  Crown Point, IN
+                </Header>
                 {/* <br/>
                 <Header as="h6">JonathanJamelHolloway@gmail.com</Header> */}
-                <br/>
-                <a href="https://github.com/codejoncode" target="_blank" rel ="noopener noreferrer">
-                <Icon   name="github" />
+                <br />
+                <a
+                  href="https://github.com/codejoncode"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon name="github" />
                 </a>
 
-                <a href="https://twitter.com/Jonathanjamelh1" target="_blank" rel ="noopener noreferrer">
-                <Icon   name="linkedin" />
+                <a
+                  href="https://twitter.com/Jonathanjamelh1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon name="linkedin" />
                 </a>
 
-                <a href="https://www.linkedin.com/in/jonathanjholloway/" target="_blank" rel ="noopener noreferrer">
-                <Icon  name="twitter" />
+                <a
+                  href="https://www.linkedin.com/in/jonathanjholloway/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon name="twitter" />
                 </a>
-                
-                
-                
-
               </Item.Content>
             </Item>
           </Item.Group>
           {/* <IntroductionSkills /> */}
         </Segment>
         <Grid columns={3} stackable>
-          <Grid.Row>
+          <Grid.Row style={{ margin: "40px" }}>
             <Grid.Column>
-              <Card>
-              <div style = {{textAlign: "center"}}>
-                <Icon name = "desktop" size= "huge"/>
-              
-              </div>
-                <Card.Content>
-                <Card.Header>FRONTEND</Card.Header>
-                <Card.Description>
-
-                </Card.Description>
-
-                </Card.Content>
-
-              </Card>
-            </Grid.Column>
-
-            <Grid.Column>
-              <Card>
-              <div style = {{textAlign: "center"}}>
-              
-              <Icon name = "database" size= "huge"/>
-              </div>
-              <Card.Content>
-                <Card.Header>BACKEND</Card.Header>
-                <Card.Description>
-                  
-                </Card.Description>
-
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-
-            <Grid.Column>
-              <Card>
-                <div style = {{textAlign: "center"}}>
-              <Icon name = "bolt" size= "huge" />
-
+              <Card style = {{backgroundColor: "#C5C6C7"}}>
+                <div style={{ textAlign: "center" }}>
+                  <Icon name="desktop" size="huge" style= {{color: "#66FCF1"}}/>
                 </div>
-              <Card.Content>
-                <Card.Header>OTHERS</Card.Header>
-                <Card.Description>
-                  
-                </Card.Description>
+                <Card.Content>
+                  <Card.Header>FRONTEND</Card.Header>
+                  <Card.Description>
+                    <List>
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>React/Redux</List.Header>
+                        </List.Content>
+                      </List.Item>
 
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>JavaScript</List.Header>
+                        </List.Content>
+                      </List.Item>
+                    
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Node.js</List.Header>
+                        </List.Content>
+                      </List.Item>
+                    
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>HTML/CSS/Less</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Styled Components</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+
+                    </List>
+                  </Card.Description>
                 </Card.Content>
               </Card>
             </Grid.Column>
-            
+
+            <Grid.Column>
+              <Card style = {{backgroundColor: "#C5C6C7"}}>
+                <div style={{ textAlign: "center" }}>
+                  <Icon name="database" size="huge" style= {{color: "#66FCF1"}}/>
+                </div>
+                <Card.Content>
+                  <Card.Header>BACKEND</Card.Header>
+                  <Card.Description>
+                    <List>
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Python/Django</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Firebase</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>PostgresSQL</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Sqlite3</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Express</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+
+
+                    </List>
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Card style = {{backgroundColor: "#C5C6C7"}}>
+                <div style={{ textAlign: "center" }}>
+                  <Icon name="bolt" size="huge" style= {{color: "#66FCF1"}} />
+                </div>
+                <Card.Content>
+                  <Card.Header>OTHERS</Card.Header>
+                  <Card.Description>
+                    <List>
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Unit Testing</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Project Management</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Coding Teaching Assistance</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Customer Service Professional</List.Header>
+                        </List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        {/* <Icon name="check" style= {{color: "#66FCF1"}}/>
+                        <List.Content>
+                          <List.Header>Customer Service Professional</List.Header>
+                        </List.Content> */}
+                      </List.Item>
+
+                      
+                    </List>
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
           </Grid.Row>
         </Grid>
       </Grid.Column>
+      </Container>
     );
   }
 }
