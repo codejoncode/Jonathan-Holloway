6d471054c07f5f9148f25ea30d277b41e118af98
codejoncode <jonathanjholloway@gmail.com>
2019-06-08 16:58:15 -0500

    fixed the styling of the admin login page

--- client/src/Components/Admin/SignIn.js
+++ client/src/Components/Admin/SignIn.js
@@ -1,40 +1,59 @@
 import React, { Component } from "react";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Grid, Container, Input } from "semantic-ui-react";
 
 class SignIn extends Component {
   state = {
     username: "",
     password: "",
-    isAuthenticated: false,
+    isAuthenticated: false
   };
 
-  handleChange = (e) => {
-    e.preventDefault()
-    this.setState({[e.target.name]: e.target.value})
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   handleSubmit = () => {
     /*this is where I call an action  */
-    const { username, password} = this.state; 
-  }
+    const { username, password } = this.state;
+  };
 
   render() {
+    const {username, password} = this.state; 
     return (
-     <Form inverted>
-       <Form.Field>
-        <label>Username</label>
-        <input placeholder = "username" onChange = {this.handleChange}/>
-       </Form.Field>
+      <Container>
+        <Grid columns={1}>
+          <Grid.Row>
+            <Grid.Column>
+              <Form inverted>
+                <Form.Field
+                    required
+                    placeholder="Enter username"
+                    label="Username"
+                    name = 'username'
+                    value = {username}
+                    onChange = {this.handleChange}
+                    control={Input}
+                  />
 
-       <Form.Field>
-         <label>Password</label>
-         <input type="password" onChange = {this.handleChange}/>
+                  <Form.Field
+                    required
+                    placeholder="Enter username"
+                    label="Password"
+                    name = 'password'
+                    value = {password}
+                    onChange = {this.handleChange}
+                    type= "password"
+                    control={Input}
+                  />
 
-       </Form.Field>
-
-       <Button type='submit'>Register</Button>
-       <Button type ='submit'>Login</Button>
-     </Form>
+                <Button type="submit">Register</Button>
+                <Button type="submit">Login</Button>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
