d91621cb6e6398331547e2365a05ef2eb199ea57
codejoncode <jonathanjholloway@gmail.com>
2019-06-09 23:08:07 -0500

    now getting projects from the backend  a little slow going from a single project back to all projects, also another issue I notice is the tab isn't filtering the data still a few more things to adjust from the data being used on the frontend

--- client/src/Components/Projects/ProjectDetails.js
+++ client/src/Components/Projects/ProjectDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux"; 
 import { withRouter } from "react-router";
 import {
   Header,
@@ -22,56 +23,79 @@ import {
   anotherBlue
 } from "../../Helpers/Colors/colors";
 
+import { fetchOneProject } from "../../Store/Actions/projectActions";
+
+
+const mapState = state => ({
+  project: state.projectReducer, 
+});
+
+const actions = {
+  fetchOneProject,
+}
+
 class ProjectDetails extends Component {
   state = {
     project: null
   };
   componentWillMount() {
     const { id } = this.props.match.params;
-    const project = this.props.projectsDisplay.filter(
-      pj => pj.id === Number(id)
-    )[0];
-    this.setState({ project });
+    // const project = this.props.projectsDisplay.filter(
+    //   pj => pj.id === Number(id)
+    // )[0];
+    this.props.fetchOneProject(id);
+    // this.setState({ project });
   }
+
+  goGetProject = async (id) => {
+    await this.props.fetchOneProject(id);
+    
+  }
+
   render() {
-    const { project } = this.state;
-    return (
-      <div>
-        <Segment textAlign="center" style={{ backgroundColor: darkBlack }}>
-          <Item>
-            <Header style={{ color: lighterBlue }}>{project.title}</Header>
-            {/* <div style = {{display : "flex", justifyContent: "center"}}>
-            <p style={{ color: lighterBlue, width : "65%", textAlign: "center"}}>
-              {project.description}
-            </p>
-
-            </div> */}
-            <Image size="big" src={project.image} centered />
-          </Item>
-        </Segment>
-
-        <Grid >
-          <Grid.Row>
-
-          <Message style={{ backgroundColor: darkBlack, padding: "40px", marginTop: "14px", boxShadow: "none"}}>
-            <Header style={{ color: lighterBlue }}>Features Implemented</Header>
-            <ProjectFeatureList
-              features={project.features}
-              lighterBlue={lighterBlue}
-            />
-          </Message>
-          <Message style={{ backgroundColor: darkBlack, padding: "40px" , boxShadow: "none"  }}>
-            <GithubLinks links={project.githubUrl} lighterBlue={lighterBlue} />
-            <PlanLinks links={project.plan} lighterBlue={lighterBlue} />
-            <DeploymentLinks
-              links={project.deployUrl}
-              lighterBlue={lighterBlue}
-            />
-          </Message>
-          </Grid.Row>
-        </Grid>
-      </div>
-    );
+    console.log(this.props)
+    const project = this.props.project ? this.props.project[0] : null;
+    console.log(project)
+    if (project ) {
+      return (
+         <div>
+          <Segment textAlign="center" style={{ backgroundColor: darkBlack }}>
+            <Item>
+              <Header style={{ color: lighterBlue }}>{project.title}</Header>
+              {/* <div style = {{display : "flex", justifyContent: "center"}}>
+              <p style={{ color: lighterBlue, width : "65%", textAlign: "center"}}>
+                {project.description}
+              </p>
+  
+              </div> */}
+              <Image size="big" src={project.image} centered />
+            </Item>
+          </Segment>
+  
+          <Grid >
+            <Grid.Row>
+  
+            <Message style={{ backgroundColor: darkBlack, padding: "40px", marginTop: "14px", boxShadow: "none"}}>
+              <Header style={{ color: lighterBlue }}>Features Implemented</Header>
+              <ProjectFeatureList
+                features={project.features}
+                lighterBlue={lighterBlue}
+              />
+            </Message>
+            <Message style={{ backgroundColor: darkBlack, padding: "40px" , boxShadow: "none"  }}>
+              <GithubLinks links={project.githubUrl} lighterBlue={lighterBlue} />
+              <PlanLinks links={project.plan} lighterBlue={lighterBlue} />
+              <DeploymentLinks
+                links={project.deployUrl}
+                lighterBlue={lighterBlue}
+              />
+            </Message>
+            </Grid.Row>
+          </Grid>
+        </div>
+      );
+    } 
+    return <div>Loading</div>
   }
 }
-export default withRouter(ProjectDetails);
+export default withRouter(connect(mapState, actions)(ProjectDetails));
--- client/src/Components/Projects/Projects.js
+++ client/src/Components/Projects/Projects.js
@@ -3,85 +3,175 @@ import { connect } from "react-redux";
 import { Grid, Menu, Header, Button, Popup, Confirm } from "semantic-ui-react";
 import { technologiesData } from "../../Helpers/projectData";
 import ProjectSection from "./ProjectSection";
-import { fetchOneProject, fetchProjects} from "../../Store/Actions/projectActions";
-const timeoutLength = 10000; 
+import {
+  fetchOneProject,
+  fetchProjects
+} from "../../Store/Actions/projectActions";
+const timeoutLength = 10000;
 
 const mapState = state => ({
   projects: state.projectReducer
-})
+});
 
 const actions = {
   fetchOneProject,
-  fetchProjects,
-}
+  fetchProjects
+};
 
 class Projects extends Component {
-  state = { isOpen : true, open: true, popUpStyle: {backgroundColor: "#66FCF1", color: "#0B0C10", fontWeight: "900"}, intialized: false}
+  state = {
+    isOpen: true,
+    open: true,
+    popUpStyle: {
+      backgroundColor: "#66FCF1",
+      color: "#0B0C10",
+      fontWeight: "900"
+    },
+    intialized: false,
+    columnCount: 5,
+    activeItem: "ALL",
+    projectsDisplay: [], 
+    technologies : [],
+  };
 
-  componentDidMount () {
+  componentDidMount() {
     this.handleOpen();
     this.props.fetchProjects();
   }
 
-  componentWillUpdate () {
-    if(this.state.intialized === false && this.props.projects.length > 0){
-      console.log(this.props.projects)
+  componentWillUpdate() {
+    if (this.state.intialized === false && this.props.projects.length > 0) {
+      console.log(this.props.projects);
+      this.filterData(this.props.projects, "ALL")
     }
   }
 
+  filterData = async (projects, name) => {
+    const rowCount = Math.ceil(projects.length / this.state.columnCount);
+    const projectsDisplay = [];
+    let projectCount = 0;
+
+    for (let i = 0; i < rowCount; i++) {
+      let setOfColumns = [];
+      while (projectCount < projects.length) {
+        if (projects[projectCount]) {
+          const tech = projects[projectCount].technologies.toUpperCase();
+          if (
+            this.state.activeItem === "ALL" ||
+            tech.includes(this.state.activeItem) === true
+          ) {
+            setOfColumns.push(projects[projectCount]);
+            if (setOfColumns.length === this.state.columnCount) {
+              projectCount += 1;
+              break;
+            }
+          }
+        }
+        projectCount += 1;
+      }
+      if (setOfColumns.length > 0) {
+        projectsDisplay.push(setOfColumns);
+      }
+      if (projectCount >= projects.length) {
+        break;
+      }
+    }
+
+    const technologiesData = [];
+    for (let project of projects) {
+      const tech = project.technologies.split(" ");
+      for (let t of tech) {
+        if (technologiesData.includes(t.toUpperCase()) === false && t.length > 0) {
+          technologiesData.push(t.toUpperCase());
+        }
+      }
+}
+
+technologiesData.sort();
+    this.setState({ projectsDisplay, activeItem: name, intialized: true, technologies: technologiesData });
+     
+  };
+
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name }, () =>
+      this.filterData(name.toUpperCase())
+    );
+  };
 
   handleOpen = () => {
-    this.setState({ isOpen: true })
+    this.setState({ isOpen: true });
 
     this.timeout = setTimeout(() => {
-      this.setState({ isOpen: false })
-    }, timeoutLength)
-  }
+      this.setState({ isOpen: false });
+    }, timeoutLength);
+  };
 
   handleClose = () => {
-    this.setState({ isOpen: false })
-    clearTimeout(this.timeout)
-  }
-  handleConfirm = () => this.setState({ open: false })
-  handleCancel = () => this.setState({ open: false })
+    this.setState({ isOpen: false });
+    clearTimeout(this.timeout);
+  };
+  handleConfirm = () => this.setState({ open: false });
+  handleCancel = () => this.setState({ open: false });
 
   render() {
-    const technologies = ["ALL", ...technologiesData];
-    const { handleOpen, handleClose, modalOpen, currentModal, projectsDisplay, activeItem, columnCount, handleItemClick, goToProjectPage,darkBlack,lightBlack,grey,lighterBlue,anotherBlue } = this.props;
-    const { isOpen, open, popUpStyle} = this.state; 
-    console.log(this.props.projects)
+    // const technologies = ["ALL", ...technologiesData];
+    const technologies = ["ALL", ...this.state.technologies]
+    const {
+      handleOpen,
+      handleClose,
+      modalOpen,
+      currentModal,
+      // projectsDisplay,
+      activeItem,
+      columnCount,
+      // handleItemClick,
+      goToProjectPage,
+      darkBlack,
+      lightBlack,
+      grey,
+      lighterBlue,
+      anotherBlue
+    } = this.props;
+    const { isOpen, open, popUpStyle, projectsDisplay } = this.state;
+    console.log(this.state.projectsDisplay);
     return (
       <div style={{ margin: "20px" }}>
-        <Menu pagination fluid stackable id ="navTagsBlue" style = {{background: darkBlack}}>
+        <Menu
+          pagination
+          fluid
+          stackable
+          id="navTagsBlue"
+          style={{ background: darkBlack }}
+        >
           {technologies.map((tech, index) => (
             <Menu.Item
               key={index}
               name={tech.toUpperCase()}
               active={activeItem === tech.toUpperCase()}
-              onClick={handleItemClick}
+              onClick={this.handleItemClick}
             />
           ))}
         </Menu>
-        
-          <Popup 
-        content = {"Hover over a Project for a description. \n\n Click a tab to filter the projects list. \n\n Click a project for more information."}
-        open = {isOpen}
-        onClose = {this.handleClose}
-        onOpen= {this.handleOpen}
-        position = 'top center'
-        trigger={<div></div>}
-        style = {popUpStyle}
+
+        <Popup
+          content={
+            "Hover over a Project for a description. \n\n Click a tab to filter the projects list. \n\n Click a project for more information."
+          }
+          open={isOpen}
+          onClose={this.handleClose}
+          onOpen={this.handleOpen}
+          position="top center"
+          trigger={<div />}
+          style={popUpStyle}
         />
-        
+
         <Grid
           style={{ margin: "20px" }}
           columns={columnCount}
           divided
           stackable
         >
-
           {projectsDisplay.map((projects, index) => (
-            
             <ProjectSection
               key={index}
               projects={projects}
@@ -89,12 +179,12 @@ class Projects extends Component {
               handleClose={handleClose}
               modalOpen={modalOpen}
               currentModal={currentModal}
-              goToProjectPage = {goToProjectPage}
-              darkBlack = {darkBlack}
-              lightBlack = {lightBlack} 
-              grey = {grey}
-              lighterBlue = {lighterBlue} 
-              anotherBlue = {anotherBlue}
+              goToProjectPage={goToProjectPage}
+              darkBlack={darkBlack}
+              lightBlack={lightBlack}
+              grey={grey}
+              lighterBlue={lighterBlue}
+              anotherBlue={anotherBlue}
             />
           ))}
         </Grid>
@@ -102,4 +192,7 @@ class Projects extends Component {
     );
   }
 }
-export default connect(mapState, actions)(Projects);
+export default connect(
+  mapState,
+  actions
+)(Projects);
